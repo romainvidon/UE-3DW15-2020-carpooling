79b930696e36ac567cb2433694c72f2528bb85b0
Romain <romain.vidon@laposte.net>
2020-11-16 16:08:07 +0100

    main: Created Ad class

---
+++ class/Entities/Ad.php
@@ -0,0 +1,112 @@
+<?php
+
+class Ad
+{
+    private $id;
+    private $title;
+    private $description;
+    private $user_id;
+    private $car_id;
+
+    
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user_id
+     */
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set the value of user_id
+     *
+     * @return  self
+     */
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of car_id
+     */
+    public function getCar_id()
+    {
+        return $this->car_id;
+    }
+
+    /**
+     * Set the value of car_id
+     *
+     * @return  self
+     */
+    public function setCar_id($car_id)
+    {
+        $this->car_id = $car_id;
+
+        return $this;
+    }
+}
