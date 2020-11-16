7fe150ee59a02ccdcc2699e77680512532784f7a
Romain <romain.vidon@laposte.net>
2020-11-16 16:25:01 +0100

    main: Created Car class

---
+++ class/Entities/Car.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entities;
+
+class Car
+{
+    private $id;
+    private $model;
+    private $user_id;
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of model
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set the value of model
+     *
+     * @return  self
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user_id
+     */
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set the value of user_id
+     *
+     * @return  self
+     */
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
