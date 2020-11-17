7fc7ff532ef47cf93a73fdcae81908799238146e
Romain <romain.vidon@laposte.net>
2020-11-17 21:03:34 +0100

    main: Ads create and update test pages

---
+++ ads_create.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Controllers\AdsController;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$controller = new AdsController();
+echo $controller->createAd();
+?>
+
+<p>Création d'une annonce</p>
+<form method="post" action="ads_create.php" name ="adCreateForm">
+    <label for="title">Titre</label>
+    <input type="text" name="title" id="title">
+    <br />
+    <label for="description">Description</label>
+    <textarea name="description" id="description"></textarea>
+    <br />
+    <label for="user_id">Identifiant de l'utilisateur :</label>
+    <input type="number" name="user_id" id="user_id">
+    <br />
+    <label for="car_id">Identifiant de la voiture :</label>
+    <input type="number" name="car_id" id="car_id">
+    <br />
+    <input type="submit" value="Créer une annonce">
+</form>
